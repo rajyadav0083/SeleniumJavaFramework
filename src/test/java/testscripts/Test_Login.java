a33416f4ce45370990a87689c1f39c4d5563ac63
rajya <rajya@Priyaansh>
2024-02-20 01:06:12 +0530

    Added new project

--- src/test/java/testscripts/Test_Login.java
+++ src/test/java/testscripts/Test_Login.java
@@ -1,40 +1,56 @@
 package test.java.testscripts;
 
-import org.openqa.selenium.Alert;
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import java.util.concurrent.TimeUnit;
+//import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test_Login {
 
 	/**
 	 * @param args
-	 * @return 
+	 * @return
 	 * @throws InterruptedException
 	 */
 	@Test
 	public void Test1(){
 		EdgeDriver driver=new EdgeDriver();
+		Logger log = LogManager.getLogger(Test_Login.class);
 		try {
+			log.info("Test started");
 			driver.navigate().to("https://www.toolsqa.com/");
 			driver.manage().window().maximize();
-			
-			Thread.sleep(1000);
-			
+
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.findElement(By.className("navbar__tutorial-menu--text")).click();
-			Thread.sleep(1000);
+
+			WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'QA Practices')]")));
 			driver.findElement(By.xpath("//span[contains(text(),'QA Practices')]")).click();
+
+			String text1= driver.findElement(By.xpath("//span[contains(text(),'QA Practices')]")).getText();
+			log.info("Text get: "+ text1);
+			Assert.assertTrue(text1.equals("QA Practices"), "Text not matched successfully");
+			
 			Thread.sleep(3000);
 			//driver.findElement(By.xpath("/html/body/nav/div/div/div[2]/div/ul/li[3]/a']")).click();
-			
-			 //Alert alert= driver.switchTo().alert(); 
+
+			 //Alert alert= driver.switchTo().alert();
 			 //alert.accept();
 			 //System.out.println("Text::"+text);
-			System.out.println("Test case executed successfully");
-			 
-			driver.close();	
+			log.info("Test case executed successfully");
+
+			driver.close();
 		} catch (Exception e) {
-			System.out.println("Exception@@@@@@@@@@@@"+e);
+			log.error("Exception@@@@@@@@@@@@"+e);
 		}
 
 	}
